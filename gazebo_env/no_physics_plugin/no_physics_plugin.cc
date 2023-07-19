3ee3c2df9181fb59c4f2f05dfad526f955853413
kushtimusPrime <kushtimusprime@gmail.com>
2023-07-19 15:33:05 -0700

    ROS Integration with plugins. Listens to chatter topic

--- gazebo_env/no_physics_plugin/no_physics_plugin.cc
+++ gazebo_env/no_physics_plugin/no_physics_plugin.cc
@@ -3,6 +3,9 @@
 
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
+#include <gazebo_ros/node.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
 
 namespace gazebo
 {
@@ -19,10 +22,26 @@ namespace gazebo
     /// \param[in] _sdf A pointer to the plugin's SDF element.
     public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
     {
+      // Initialize ROS node
+      this->node_ = gazebo_ros::Node::Get(_sdf);
+      const gazebo_ros::QoS &qos = this->node_->get_qos();
       // Just output a message for now
       std::cerr << "NO PHYSICS" << "\n";
       _model->GetWorld()->SetPhysicsEnabled(false);
+      this->chatter_subscriber_ = this->node_->create_subscription<std_msgs::msg::String>(
+                "chatter",
+                qos.get_subscription_qos("chatter", rclcpp::QoS(1)),
+                std::bind(&NoPhysicsPlugin::ChatterMsg, this, std::placeholders::_1));
     }
+
+    void ChatterMsg(const std_msgs::msg::String::SharedPtr msg) {
+        std::cout << "COOKING" << std::endl;
+    }
+
+
+    private:
+      gazebo_ros::Node::SharedPtr node_;
+      rclcpp::Subscription<std_msgs::msg::String>::SharedPtr chatter_subscriber_;
   };
 
   // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
