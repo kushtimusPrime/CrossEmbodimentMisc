d5ecc45b54b72cee60f44876c43ee50694a056a0
kushtimusPrime <kushtimusprime@gmail.com>
2023-07-19 11:57:10 -0700

    About to add gazebo

--- fk_ik_rviz_pkg/scripts/forward_test.py
+++ fk_ik_rviz_pkg/scripts/forward_test.py
@@ -1,15 +1,19 @@
 import compas_fab
 from compas.robots import Configuration
 from compas_fab.backends import PyBulletClient
+import pathlib
+import time
 
 with PyBulletClient() as client:
     # TODO: Change hardcoded Python file
-    urdf_filename = compas_fab.get('../../../../../../kinpy_tests/test_model.urdf')
+    file_path = pathlib.Path(__file__).parent.resolve()
+    urdf_filename = compas_fab.get('../../../../../../cross_embodiment_test_ws/src/fk_ik_rviz_pkg/scripts/pybullet_ur5_robotiq/pybullet_ur5_robotiq/urdf/ur5_robotiq_85.urdf')
     robot = client.load_robot(urdf_filename)
 
-    configuration = Configuration.from_revolute_values([0.5,0.5])
+    configuration = Configuration.from_revolute_values([0,0,0,0,0,0,0])
 
     frame_WCF = robot.forward_kinematics(configuration)
 
+    # Given 4x4 matrix, it is 4th column (position), first column, and then second column
     print("Frame in the world coordinate system")
     print(frame_WCF)
--- fk_ik_rviz_pkg/scripts/inverse_test.py
+++ fk_ik_rviz_pkg/scripts/inverse_test.py
@@ -3,11 +3,11 @@ from compas.geometry import Frame
 from compas_fab.backends import PyBulletClient
 
 with PyBulletClient() as client:
-    urdf_filename = compas_fab.get('../../../../../../kinpy_tests/test_model.urdf')
+    urdf_filename = compas_fab.get('../../../../../../cross_embodiment_test_ws/src/fk_ik_rviz_pkg/scripts/panda.urdf')
     robot = client.load_robot(urdf_filename)
 
     # Given 4x4 matrix, it is 4th column (position), first column, and then second column
-    frame_WCF = Frame([-0.848,0.200,2.251], [0.462,0,-0.887], [0, 1, 0])
+    frame_WCF = Frame([0.088, 0.000, 0.926], [1,0,0], [0, -1,0])
     start_configuration = robot.zero_configuration()
 
     configuration = robot.inverse_kinematics(frame_WCF, start_configuration)
